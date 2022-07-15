27d2d1f52e352556d16b67fa17f58f2f2018d41c
jeffrey2423 <riosjeff24@gmail.com>
2022-07-15 07:58:29 -0500

    Mejoras generales en el front

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react'
-import Spinner from '../components/Spinner'
-import { api } from '../api/api';
-import { mostrarMensaje, DescargarArchivo, BlobToText } from '../utils/Utils';
+import React, { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import { api } from "../api/api";
+import {
+  mostrarMensaje,
+  DescargarArchivo,
+  BlobToText,
+  IsUndefinedOrNullOrEmptyOrFalse,
+} from "../utils/Utils";
 
 const Index = () => {
-
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
+  const [btnSlaOpRecDisabled, setBtnSlaOpRecDisabled] = useState(true);
+  const [btnSlaOpDicDisabled, setBtnSlaOpDicDisabled] = useState(true);
+  const [btnCopLibRecDisabled, setBtnCopLibRecDisabled] = useState(true);
+  const [btnCopLibDicDisabled, setBtnCopLibDicDisabled] = useState(true);
+
+  useEffect(() => {
+    setBtnSlaOpRecDisabled(true);
+    setBtnSlaOpDicDisabled(true);
+    setBtnCopLibRecDisabled(true);
+    setBtnCopLibDicDisabled(true);
+  }, []);
 
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
-  }
+    if (!IsUndefinedOrNullOrEmptyOrFalse(e.target.files[0])) {
+      setBtnSlaOpRecDisabled(false);
+      setBtnSlaOpDicDisabled(false);
+      setBtnCopLibRecDisabled(false);
+      setBtnCopLibDicDisabled(false);
+    }
+  };
 
   const handleApiFileUpload = async (endpoint) => {
     let response;
@@ -22,21 +43,19 @@ const Index = () => {
 
       try {
         responseText = JSON.parse(await BlobToText(response.data));
-      } catch (error) {       
-      }
-    
+      } catch (error) {}
+
       if (responseText.error_status) {
-        throw new Error("Ha ocurrido un error "+responseText.error_message);
-      }else{
+        throw new Error("Ha ocurrido un error " + responseText.error_message);
+      } else {
         DescargarArchivo(response.data, endpoint);
       }
- 
     } catch (error) {
       mostrarMensaje.Error(error.message);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   return (
     <>
@@ -45,8 +64,16 @@ const Index = () => {
         <div className="row">
           <div className="col">
             <div>
-              <label htmlFor='formFileLg' className="form-label">Selecciona el archivo con la informacion del problema</label>
-              <input onChange={onFileChange} className="form-control form-control-lg" id="formFileLg" type="file" name='file' />
+              <label htmlFor="formFileLg" className="form-label">
+                Selecciona el archivo con la informacion del problema
+              </label>
+              <input
+                onChange={onFileChange}
+                className="form-control form-control-lg"
+                id="formFileLg"
+                type="file"
+                name="file"
+              />
             </div>
           </div>
         </div>
@@ -54,23 +81,33 @@ const Index = () => {
           <div className="col">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Optimizando el uso de una sala de operaciones</h5>
-                <p className="card-text">El hospital desea seleccionar cu´ales procedimientos
-                  quirurgicos se van a determinar en una
-                  sala, para esto se busca asignar los procedimientos
-                  a la sala de tal forma que no hayan
-                  cruces en los horarios seleccionados y se maximice
-                  el tiempo que la sala se encuentre en
-                  funcionamiento en el día.</p>
+                <h5 className="card-title">
+                  Optimizando el uso de una sala de operaciones
+                </h5>
+                <p className="card-text">
+                  El hospital desea seleccionar cu´ales procedimientos
+                  quirurgicos se van a determinar en una sala, para esto se
+                  busca asignar los procedimientos a la sala de tal forma que no
+                  hayan cruces en los horarios seleccionados y se maximice el
+                  tiempo que la sala se encuentre en funcionamiento en el día.
+                </p>
                 <div className="d-flex justify-content-between">
                   <button
-                    onClick={() => handleApiFileUpload('SalaOperaciones/SolucionRecursiva')}
-                    className="btn btn-primary">
+                    disabled={btnSlaOpRecDisabled}
+                    onClick={() =>
+                      handleApiFileUpload("SalaOperaciones/SolucionRecursiva")
+                    }
+                    className="btn btn-primary"
+                  >
                     Solucion Recursiva
                   </button>
-                  <button 
-                    onClick={() => handleApiFileUpload("SalaOperaciones/SolucionDinamica")}
-                    className="btn btn-primary">
+                  <button
+                    disabled={btnSlaOpDicDisabled}
+                    onClick={() =>
+                      handleApiFileUpload("SalaOperaciones/SolucionDinamica")
+                    }
+                    className="btn btn-primary"
+                  >
                     Solucion Dinamica
                   </button>
                 </div>
@@ -82,17 +119,27 @@ const Index = () => {
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Copia de libros</h5>
-                  <p className="card-text">Determinar como repartir
-                    los libros entre los escritores de forma que
-                    el tiempo que se demoren realizando las copias
-                    sea el mínimo teniendo en cuenta que el tiempo
-                    que se demoran en realizar las copias depende
-                    del escritor que tenga mas paginas asignadas.
-                    Hacer la copia de una pagina tiene una duraci
-                    on de un dia.</p>
+                  <p className="card-text">
+                    Determinar como repartir los libros entre los escritores de
+                    forma que el tiempo que se demoren realizando las copias sea
+                    el mínimo teniendo en cuenta que el tiempo que se demoran en
+                    realizar las copias depende del escritor que tenga mas
+                    paginas asignadas. Hacer la copia de una pagina tiene una
+                    duraci on de un dia.
+                  </p>
                   <div className="d-flex justify-content-between">
-                    <button className="btn btn-primary">Solucion Recursiva</button>
-                    <button className="btn btn-primary">Solucion Dinamica</button>
+                    <button
+                      disabled={btnCopLibRecDisabled}
+                      className="btn btn-primary"
+                    >
+                      Solucion Recursiva
+                    </button>
+                    <button
+                      disabled={btnCopLibDicDisabled}
+                      className="btn btn-primary"
+                    >
+                      Solucion Dinamica
+                    </button>
                   </div>
                 </div>
               </div>
@@ -101,6 +148,6 @@ const Index = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 export default Index;
